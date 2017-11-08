48fc69503e8f0d9257baaa9bb68fdbdc2c423e37
Ben Scholer <benscholer3248511@gmail.com>
2017-11-08 12:27:43 -0500

    Added program description, and rearanged methods

--- lab09/main.c
+++ lab09/main.c
@@ -11,10 +11,56 @@ int octin;
 //Initial number of digits
 int digits;
 
+/* How the program works:
+ * User inputs an octal number.
+ * Check if it's a valid number, if not, keep asking until it is.
+ * Find number of digits
+ * Interate through a for loop once for every digit
+ * Print the stuff, with as many digits as there were origianlly to account for leading 0's.
+ * Convert the octal number to decimal by iterating through each digit and multiplying it by 10^i, and adding that to the final decimal number.
+ * Find each digit of the hexadecimal representation of the decimal number.
+ * Store the largest hex digit as you find them, and return this.
+ * Print that out, as it is the largest hex digit in the octal number.
+ * Move the most important octal digit to be the least important. BE CAREFUL ABOUT LEADING 0's!!!!
+ * To account for leading 0's, use the number of digits of the original octal number to see if they match.
+ * If they don't, the digit that should be moved is 0, instead of the actual most important digit.
+ */
+
 int main()
 {
     inputOctin();
     iterator(octin);
+}
+
+void inputOctin()
+{
+    do {
+        printf("Enter octal number: ");
+        scanf("%d", &octin);
+        if (octin < 0) {
+            printf("\nError! Negative numbers are invalid!!\n\n");
+        } else if (checkOctal(octin) == 0) {
+            //An octal error will only be flagged if octin < 0 by putting it in an else if.
+            printf("\nError! Input contains invalid digit!!\n\n");
+        }
+    } while (octin < 0 || checkOctal(octin) == 0);
+    digits = floor(log10(octin)) + 1;
+    printf("\n");
+}
+
+//return a 0 or 1 if number is invalid or valid, respectively.
+int checkOctal(int number)
+{
+    int i;
+    for (i = 0; i < digits; i++) {
+        int digit = (octin / (int) pow(10, digits - i - 1)) % 10;
+        if (digit >= 8) {
+            return 0;
+        }
+    }
+    //If all of the numbers are valid octal digits, we return that the number was OK.
+    return 1;
+
 }
 
 void iterator(int num)
@@ -27,7 +73,6 @@ void iterator(int num)
         num = moveDigit(num);
     }
 }
-
 
 int octToDec(int oct)
 {
@@ -70,22 +115,6 @@ char largestHex(int dec)
     return largest;
 }
 
-void inputOctin()
-{
-    do {
-        printf("Enter octal number: ");
-        scanf("%d", &octin);
-        if (octin < 0) {
-            printf("\nError! Negative numbers are invalid!!\n\n");
-        } else if (checkOctal(octin) == 0) {
-            //An octal error will only be flagged if octin < 0 by putting it in an else if.
-            printf("\nError! Input contains invalid digit!!\n\n");
-        }
-    } while (octin < 0 || checkOctal(octin) == 0);
-    digits = floor(log10(octin)) + 1;
-    printf("\n");
-}
-
 //This function will move the leftmost digit to the rightmost position.
 int moveDigit(int num)
 {
@@ -102,19 +131,4 @@ int moveDigit(int num)
     num *= 10;
     num += firstDigit;
     return num;
-}
-
-//return a 0 or 1 if number is invalid or valid, respectively.
-int checkOctal(int number)
-{
-    int i;
-    for (i = 0; i < digits; i++) {
-        int digit = (octin / (int) pow(10, digits - i - 1)) % 10;
-        if (digit >= 8) {
-            return 0;
-        }
-    }
-    //If all of the numbers are valid octal digits, we return that the number was OK.
-    return 1;
-
 }
